4a2ae06658ca256df04ce57b83af05285709d32f
RobinSanta <robinsntn@gmail.com>
2019-11-06 10:45:56 -0800

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,46 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    self.paintings.map {|painting| painting.gallery.city}.uniq
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def avg_paintings_per_year
+    num_of_paintings = Painting.all.select {|painting| painting.artist == self}.length.to_f
+    num_of_years = self.years_experience.to_f
+    num_of_paintings / num_of_years
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    @@all.map {|artist| artist.years_experience}.sum
+  end
+
+  def self.most_prolific
+    @@all.max_by {|artist| artist.avg_paintings_per_year}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,22 @@
 require_relative '../config/environment.rb'
 
 
+artist_1 = Artist.new("Artist_1", 10)
+artist_2 = Artist.new("Artist_2", 5)
+artist_3 = Artist.new("Artist_3", 40)
+artist_4 = Artist.new("Artist_4", 27)
 
+gallery_1 = Gallery.new("Gallery_1", "New York")
+gallery_2 = Gallery.new("Gallery_2", "Seattle")
+
+painting_1 = Painting.new("Painting_1", 10000, artist_1, gallery_1)
+painting_2 = Painting.new("Painting_2", 90000, artist_1, gallery_2)
+painting_3 = Painting.new("Painting_3", 9000, artist_2, gallery_1)
+painting_4 = Painting.new("Painting_4", 50000, artist_3, gallery_2)
+painting_5 = Painting.new("Painting_5", 30000, artist_4, gallery_1)
+painting_6 = Painting.new("Painting_6", 5000, artist_2, gallery_1)
+painting_7 = Painting.new("Painting_7", 10000, artist_4, gallery_1)
+painting_8 = Painting.new("Painting_8", 200000, artist_1, gallery_2)
 
 binding.pry
 
